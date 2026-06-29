5e595a0e7438ff3d154ebe296646e8a51f1cc6eb
Themba Maseko <thembamaseko1990@gmail.com>
2026-06-29 20:35:04 +0200

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,41 +1,34 @@
-from flask import Flask, app
+from flask import Flask
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
 from flask_mail import Mail
 
-mail = Mail()
-
 from config import Config
+
+mail = Mail()
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
 login.login_view = 'login'
-
-
 socketIO = SocketIO()
 
 
-# def create_app(config_class = Config):
-def create_app(__name__):
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     mail.init_app(app)
-
-
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
-    socketIO.init_app(app)
+    socketIO.init_app(app, cors_allowed_origins='*')
 
     from .routes import init_routes
     init_routes(app)
 
     with app.app_context():
         db.create_all()
-
 
     return app
