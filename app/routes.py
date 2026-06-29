8c12821361ddbc7010799ce804ae28c863d9024a
Themba Maseko <thembamaseko1990@gmail.com>
2026-06-29 14:12:13 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -257,6 +257,8 @@ def init_routes(app):
                 return redirect(url_for('login'))
             except:
                 db.session.rollback()
+
+                print(e)
         return render_template('register.html',form = form)
 
     @app.route('/terms')
